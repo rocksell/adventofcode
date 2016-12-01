1944b7a605c2037a205e153e5cd84b3670691351
Anastasiya <anastasya.grishina@gmail.com>
2016-12-01 16:33:16 +0400

    Remove logger

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 const data = ["L1", "L3", "L5", "L3", "R1", "L4", "L5", "R1", "R3", "L5", "R1", "L3", "L2", "L3", "R2", "R2", "L3", "L3", "R1", "L2", "R1", "L3", "L2", "R4", "R2", "L5", "R4", "L5", "R4", "L2", "R3", "L2", "R4", "R1", "L5", "L4", "R1", "L2", "R3", "R1", "R2", "L4", "R1", "L2", "R3", "L2", "L3", "R5", "L192", "R4", "L5", "R4", "L1", "R4", "L4", "R2", "L5", "R45", "L2", "L5", "R4", "R5", "L3", "R5", "R77", "R2", "R5", "L5", "R1", "R4", "L4", "L4", "R2", "L4", "L1", "R191", "R1", "L1", "L2", "L2", "L4", "L3", "R1", "L3", "R1", "R5", "R3", "L1", "L4", "L2", "L3", "L1", "L1", "R5", "L4", "R1", "L3", "R1", "L2", "R1", "R4", "R5", "L4", "L2", "R4", "R5", "L1", "L2", "R3", "L4", "R2", "R2", "R3", "L2", "L3", "L5", "R3", "R1", "L4", "L3", "R4", "R2", "R2", "R2", "R1", "L4", "R4", "R1", "R2", "R1", "L2", "L2", "R4", "L1", "L2", "R3", "L3", "L5", "L4", "R4", "L3", "L1", "L5", "L3", "L5", "R5", "L5", "L4", "L2", "R1", "L2", "L4", "L2", "L4", "L1", "R4", "R4", "R5", "R1", "L4", "R2", "L4", "L2", "L4", "R2", "L4", "L1", "L2", "R1", "R4", "R3", "R2", "R2", "R5", "L1", "L2"];
 
 function getNewCoordinate(direction, x, y, length) {
-    var map = {}
+    const map = {}
     map.upL = () => ({x: x -length, y, direction: "left"});
     map.upR = () => ({x: x+length, y, direction: "right"});
     map.downL = () => ({ ...map.upR(), direction: "right"});
@@ -10,7 +10,6 @@ function getNewCoordinate(direction, x, y, length) {
     map.leftR = () => ({x, y: y + length, direction: "up"});
     map.rightL = () => ({ ...map.leftR(), direction: "up"});
     map.rightR = () => ({...map.leftL(), direction: "down"});
-    console.log(direction, x, y, length)
     return map[direction]();
 }
 
